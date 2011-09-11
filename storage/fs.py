b435564f37d8e06a7a6277e4ddf53b7f7fcbe270
TW <tw@waldmann-edv.de>
2011-09-11 15:03:03 +0200

    fs storage: more memory efficient get_file / set_file

--- storage/fs.py
+++ storage/fs.py
@@ -9,9 +9,9 @@ MoinMoin - filesystem storage
 from __future__ import absolute_import, division
 
 import os, errno, shutil
-from StringIO import StringIO
 
 from storage import MutableStorageBase
+
 
 class Storage(MutableStorageBase):
     """
@@ -37,25 +37,34 @@ class Storage(MutableStorageBase):
             yield key
 
     def get_bytes(self, key):
+        f = self.get_file(key)
         try:
-            with open(self.mkpath(key), 'rb') as f:
-                return f.read()
+            return f.read() # better use get_file() and read smaller blocks for big files
+        finally:
+            f.close()
+
+    def get_file(self, key):
+        try:
+            return open(self.mkpath(key), 'rb')
         except IOError as e:
             if e.errno == errno.ENOENT:
                 raise KeyError(key)
             raise
-
-    def get_file(self, key):
-        return StringIO(self.get_bytes(key))
 
     def set_bytes(self, key, value):
         with open(self.mkpath(key), "wb") as f:
             f.write(value)
 
     def set_file(self, key, stream):
-        value = stream.read()
-        stream.close()
-        self.set_bytes(key, value)
+        try:
+            with open(self.mkpath(key), "wb") as f:
+                blocksize = 64 * 1024
+                data = stream.read(blocksize)
+                while data:
+                    f.write(data)
+                    data = stream.read(blocksize)
+        finally:
+            stream.close()
 
     def __delitem__(self, key):
         os.remove(self.mkpath(key))
