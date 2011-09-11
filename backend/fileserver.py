858115f48fac214633a85f3342c59f084c64b3ea
TW <tw@waldmann-edv.de>
2011-09-11 20:56:59 +0200

    fileserver backend: make it non-flat, implement virtual directory wiki pages

--- backend/fileserver.py
+++ backend/fileserver.py
@@ -8,7 +8,8 @@ MoinMoin - backend exposing part of the filesystem (read-only)
 
 from __future__ import absolute_import, division
 
-import os, errno
+import os, errno, stat
+from StringIO import StringIO
 
 from backend import BackendBase
 
@@ -18,7 +19,7 @@ class Backend(BackendBase):
     exposes part of the filesystem (read-only)
     """
     def __init__(self, path):
-        self.path = path
+        self.path = unicode(path)
 
     def open(self):
         pass
@@ -30,10 +31,19 @@ class Backend(BackendBase):
         # XXX unsafe keys?
         return os.path.join(self.path, key)
 
+    def _mkkey(self, path):
+        root = self.path
+        assert path.startswith(root)
+        key = path[len(root)+1:]
+        return key
+
     def __iter__(self):
-        # TODO: flat right now, could be recursive
-        for fn in os.listdir(self.path):
-            yield fn
+        for dirpath, dirnames, filenames in os.walk(self.path):
+            key = self._mkkey(dirpath)
+            if key:
+                yield key
+            for filename in filenames:
+                yield self._mkkey(os.path.join(dirpath, filename))
 
     def get_meta(self, fn):
         path = self._mkpath(fn)
@@ -45,14 +55,64 @@ class Backend(BackendBase):
             raise
         meta = {}
         meta['mtime'] = int(st.st_mtime) # use int, not float
-        meta['size'] = int(st.st_size) # use int instead of long
+        if stat.S_ISDIR(st.st_mode):
+            # directory
+            # we create a virtual wiki page listing links to subitems:
+            ct = 'text/x.moin.wiki;charset=utf-8'
+            size = 0
+        elif stat.S_ISREG(st.st_mode):
+            # normal file
+            # TODO: real mimetype guessing
+            if fn.endswith('.png'):
+                ct = 'image/png'
+            elif fn.endswith('.txt'):
+                ct = 'text/plain'
+            else:
+                ct = 'application/octet-stream'
+            size = int(st.st_size) # use int instead of long
+        else:
+            # symlink, device file, etc.
+            ct = 'application/octet-stream'
+            size = 0
+        meta['contenttype'] = ct
+        meta['size'] = size
         return meta
+
+    def _make_directory_page(self, path):
+        try:
+            dirs = []
+            files = []
+            names = os.listdir(path)
+            for name in names:
+                filepath = os.path.join(path, name)
+                if os.path.isdir(filepath):
+                    dirs.append(name)
+                else:
+                    files.append(name)
+            content = [
+                u"= Directory contents =",
+                u" * [[../]]",
+            ]
+            content.extend(u" * [[/%s|%s/]]" % (name, name) for name in sorted(dirs))
+            content.extend(u" * [[/%s|%s]]" % (name, name) for name in sorted(files))
+            content.append(u"")
+            content = u'\r\n'.join(content)
+        except OSError as err:
+            content = unicode(err)
+        return content
 
     def get_data(self, fn):
         path = self._mkpath(fn)
         try:
-            return open(path, 'rb')
-        except IOError as e:
+            st = os.stat(path)
+            if stat.S_ISDIR(st.st_mode):
+                data = self._make_directory_page(path)
+                return StringIO(data.encode('utf-8'))
+            elif stat.S_ISREG(st.st_mode):
+                return open(path, 'rb')
+            else:
+                return StringIO('')
+        except (OSError, IOError) as e:
             if e.errno == errno.ENOENT:
                 raise KeyError(fn)
             raise
