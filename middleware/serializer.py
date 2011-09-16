fed8f372de10fa56960e5f68875a32cc68387e0d
TW <tw@waldmann-edv.de>
2011-09-16 19:18:11 +0200

    serializer/deserializer: use int 0 as indication of a valid end of stream

--- middleware/serializer.py
+++ middleware/serializer.py
@@ -1,6 +1,7 @@
 import struct
 import json
 from io import BytesIO
+
 from werkzeug.wsgi import LimitedStream
 
 
@@ -21,14 +22,17 @@ def serialize_iter(backend):
             if not block:
                 break
             yield block
+    # the deserializer expects next meta len here, but gets 0 as
+    # indication of a valid end of stream:
+    yield struct.pack('!i', 0)
 
 
 def deserialize(io, backend):
     while True:
         meta_size_bytes = io.read(4)
-        if not meta_size_bytes:
-            return
         meta_size = struct.unpack('!i', meta_size_bytes)[0]
+        if not meta_size:
+            return
         meta_str = io.read(meta_size)
         text = meta_str.decode('utf-8')
         meta = json.loads(text)
